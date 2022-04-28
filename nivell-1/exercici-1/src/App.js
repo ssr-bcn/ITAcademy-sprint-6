70f5d4404e8e82f76ebca519e35991ce3555bae2
SSR <ssr@estamosenello.net>
2022-04-28 12:42:55 +0200

    Exercici 1

---
+++ nivell-1/exercici-1/src/App.js
@@ -0,0 +1,9 @@
+import Escena from './components/escena/Escena';
+
+function App() {
+  return (
+    <Escena />
+  );
+}
+
+export default App;
---
+++ nivell-1/exercici-1/src/components/escena/Escena.js
@@ -0,0 +1,5 @@
+export default function Escena() {
+  return (
+    <>El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial</>
+  );
+}
