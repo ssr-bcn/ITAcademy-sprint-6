822a811abf7d3e1e1ea88c75470fdada71952bea
SSR <ssr@estamosenello.net>
2022-05-01 12:09:07 +0200

    Cambio para usar defaultProps

--- nivell-3/exercici-6/src/components/layout/StoryWrapper.js
+++ nivell-3/exercici-6/src/components/layout/StoryWrapper.js
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
+import { escenes } from '../escena/escenes';
 
 export const StoryWrapper = styled.section`
   width: 100%;
   height: inherit;
-  background: url('img/${props => props.bg || '0.jpg'}') center center no-repeat;
+  background: url('img/${props => props.bg}') center center no-repeat;
   background-size: cover;
 `;
+
+StoryWrapper.defaultProps = {
+  bg: escenes[0].img
+}
